67adbf76b79c202f64432529e661669efe68fb4f
zezang <zezang@gmail.com>
2022-08-19 21:42:12 -0700

    Updates to 4-natours

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv =require('dotenv');
-dotenv.config({path: './config.env'});
 const app = require('./app');
+
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(connection => {
+    console.log('DB connection successful!');
+})
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
