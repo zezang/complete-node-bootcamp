f5b6cde58018fb9fe101698d788cf4ab36fcd8ff
zezang <zezang@gmail.com>
2022-09-05 10:55:05 -0700

    added some child templates

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -55,7 +55,15 @@ app.get('/', (req, res, next) => {
 })
 
 app.get('/overview', (req, res, next) => {
-    res.status(200).render('overview');
+    res.status(200).render('overview', {
+        title: 'All Tours'
+    });
+});
+
+app.get('/tour', (req, res, next) => {
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker Tour'
+    })
 })
 
 app.use('/api/v1/tours', tourRouter);
